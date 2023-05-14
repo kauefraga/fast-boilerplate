ba0de14bc3b42e87f8b48ad88230039f9058fb5e
Kauê Fraga Rodrigues <88486000+kauefraga@users.noreply.github.com>
2023-05-13 22:36:45 -0300

    :recycle: refactor: clean up the code and solve the destructuring of the prompt

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -1,3 +1,4 @@
+import { cwd } from 'process';
 import { prompt } from 'enquirer';
 import c from 'ansi-colors';
 import { createSpinner } from 'nanospinner';
@@ -5,20 +6,17 @@ import { Git } from 'git-interface';
 import { settings } from '../core/settings';
 
 export async function main() {
-  const response = await prompt({
+  const { boilerplate } = await prompt<{ boilerplate: string }>({
     type: 'select',
     name: 'boilerplate',
-    message: 'Pick a boilerplate',
+    message: 'Choose a boilerplate to code with',
     choices: settings.boilerplate_names,
   })
-    .then((answer) => JSON.stringify(answer))
     .catch(() => {
       throw new Error(c.bold.red('Prompt canceled!'));
     });
 
-  const { boilerplate } = JSON.parse(response);
-
-  const git = new Git({ dir: process.cwd() });
+  const git = new Git({ dir: cwd() });
 
   const spinner = createSpinner('Cloning...');
 
@@ -27,13 +25,13 @@ export async function main() {
 
     await git.clone(settings.getBoilerplateUrl(boilerplate), boilerplate);
   } catch (err: any) {
-    spinner.error({ text: 'Something went wrong.' });
+    spinner.error({ text: 'Something went wrong when trying to clone.' });
     process.exit(0);
   }
 
   spinner.success();
 
   console.log(
-    c.green(`Done! You can locate it at ${c.bold.underline(`${process.cwd()}/${boilerplate}`)}`),
+    c.green(`Done! You can locate it at ${c.bold.underline(`${cwd()}/${boilerplate}`)}`),
   );
 }
